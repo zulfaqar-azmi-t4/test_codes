88ad51423fc2c2cd8fa4a59dbdc937381c5352e4
Muhammad Zulfaqar bin Azmi <zulfaqar.azmi@tier4.jp>
2022-02-08 17:22:19 +0900

    Add comments
    Signed-off-by: Muhammad Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- test_dynamic_time_warping/main.cpp
+++ test_dynamic_time_warping/main.cpp
@@ -11,11 +11,11 @@
 #include <algorithm>
 // https://www.youtube.com/watch?v=GqOIzUEZgsA
 
-struct ShiftPoint {
+struct SomeTrajectoryInfo {
   int start_idx {0};
   int end_idx {0};
 };
-using ShiftPoints = std::vector<ShiftPoint>;
+using SomeTrajectoryInfos = std::vector<SomeTrajectoryInfo>;
 
 enum class Stat : int{
   match,
@@ -32,31 +32,31 @@ struct DTWInfo {
 int main(){
 
 
-  const auto lambda = [](std::ifstream file, ShiftPoints & shift_points_){
+  const auto lambda = [](std::ifstream file, SomeTrajectoryInfos & shift_points_){
     char comma;
     std::string parsed {};
     while(getline(file, parsed)) {
       std::stringstream ss(parsed);
-      ShiftPoint sp;
+      SomeTrajectoryInfo sp;
       ss >> sp.start_idx >> comma >> sp.end_idx;
       shift_points_.push_back(sp);
     }
   };
 
-  const auto printer = [](const ShiftPoints & shifts) {
+  const auto printer = [](const SomeTrajectoryInfos & shifts) {
     for(const auto & sp:shifts){
       fmt::print("{}, {}\n", sp.start_idx, sp.end_idx);
     }
   };
-
-  ShiftPoints shift_points1;
+  
+  // 1. Read input from text file
+  SomeTrajectoryInfos shift_points1;
   lambda(std::ifstream{"../input1.txt"}, shift_points1);
-  // printer(shift_points1);
 
-  ShiftPoints shift_points2;
+  SomeTrajectoryInfos shift_points2;
   lambda(std::ifstream{"../input2.txt"}, shift_points2);
-  // printer(shift_points2);
 
+  // 2. Initialize relationship table for DTW
   Eigen::MatrixXd DTWMatrix(shift_points1.size() + 1, shift_points2.size() + 1);
   DTWMatrix.setZero();
   Eigen::MatrixXd trace_back = DTWMatrix;
@@ -70,18 +70,30 @@ int main(){
     std::size_t idx_col = 1;
 
     for(const auto & sp2:shift_points2) {
-      // const auto distance = std::hypot(sp1.start_idx - sp2.start_idx, sp1.end_idx - sp2.end_idx);
+
+      // 3. Compute relative distance between each points
+      const auto distance = std::hypot(sp1.start_idx - sp2.start_idx, sp1.end_idx - sp2.end_idx);
+
+      // 4. Set penalties which consist of match, insertion and delete
       const auto match = DTWMatrix(idx_row - 1, idx_col - 1);
       const auto insertion = DTWMatrix(idx_row - 1, idx_col);
       const auto deletion = DTWMatrix(idx_row, idx_col - 1);
       const std::vector<double> penalties {match, insertion, deletion};
+
+      // 5. Find min element in the pennalties
       const auto min_elem = std::min_element(penalties.cbegin(), penalties.cend());
+
+      // 6. Compute the cost of the relation.
       DTWMatrix(idx_row, idx_col) = distance + *(min_elem);
+
+      // 7. Take not of the status of the relation
       trace_back(idx_row, idx_col) = std::distance(penalties.cbegin(), min_elem);
       ++idx_col;
     }
     ++idx_row;
   }
+
+  // 8. Trace back the relation starting from the last element.
   DTWInfo init;
   init.row = trace_back.rows() - 1;
   init.col = trace_back.cols() - 1;
@@ -117,6 +129,8 @@ int main(){
   // fmt::print("Dynamic Time warping \n {}\n", DTWMatrix);
   std::reverse(paths.begin(), paths.end());
  
+
+  // Some printing
   paths.erase(paths.begin());
   for (const auto p : paths) {
     fmt::print("Alignment sp1 : {} < - sp2 : {} by {} \n", p.row, p.col, p.status);
